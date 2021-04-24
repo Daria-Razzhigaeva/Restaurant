b0767c88d35a6d3b2ed25134305eeb4b52fecb33
Daria Razzhigaeva <dariarazzhigaeva@Darias-MacBook-Pro.local>
2021-04-24 15:19:58 -0700

    First commit (Restaurant)

---
+++ src/takeout/Restaurant.java
@@ -0,0 +1,14 @@
+package takeout;
+
+public class Restaurant {
+
+    public static void main(String[] args) {
+
+        Menu menu = new Menu();
+        menu.printMenu();
+
+        String[] names = menu.getMainDishesNames();
+
+        MainDish newBeef = menu.getBeef();
+    }
+}
